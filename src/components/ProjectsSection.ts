3d5958fd1e85e490e9155b82367b4b7ed90982e8
Ivan Lori <ivan.lori@protonmail.com>
2020-06-07 12:11:42 +0200

    adds animation on tech stack and fixes

--- src/components/ProjectsSection.ts
+++ src/components/ProjectsSection.ts
@@ -1,35 +1,12 @@
 import { githubProfileUrl } from "./HomeSection";
 
 const urlDemoEstimate: string = "https://ivanlori.github.io/EstimateOnline/";
-const urlDemoVuemmerce: string =
-  "https://vuemmerce-git-master.ivanlori.now.sh/";
+const urlDemoVuemmerce: string = "https://vuemmerce-git-master.ivanlori.now.sh/";
 const urlCvBuilder: string = "https://github.com/ivanlori/CV-builder";
-
-const handleTabVisibility = (id: string): void => {
-  document.querySelectorAll('.js-box').forEach(el => {
-    el.classList.add('d-none');
-  });
-  document.querySelector(id).classList.remove('d-none');
-}
-
-export const handleDOM = (): void => {
-  const jsItemClass = '.js-item';
-
-  document.addEventListener('click', (e: any) => {
-    if (!e.target.matches(jsItemClass)) return;
-    e.preventDefault();
-    const target = e.target;
-
-    document.querySelectorAll(jsItemClass).forEach(el => {
-      el.classList.remove('active');
-    });
-
-    if (document.querySelector('.active') === null) {
-      handleTabVisibility(target.hash);
-      target.classList.add('active');
-    }
-  }, false);
-}
+const stackLi: string = 'js-stack-li';
+const jsTabLink: string = 'js-tab-link';
+const tabBody: string = 'js-box';
+const typeOfAnimation: string = 'fadeInRight';
 
 export const ProjectsSection: string = `
 <section class="fadeIn container">
@@ -41,45 +18,47 @@ export const ProjectsSection: string = `
     </p>
     <ul class="tab tab-block">
       <li class="tab-item">
-        <a class="js-item active" href="#builder">CV Builder</a>
+        <a class="${jsTabLink} active" href="#builder">CV Builder</a>
       </li>
       <li class="tab-item">
-        <a class="js-item" href="#vuemmerce">Vuemmerce</a>
+        <a class="${jsTabLink}" href="#vuemmerce">Vuemmerce</a>
       </li>
       <li class="tab-item">
-        <a class="js-item" href="#estimate">Estimate of costs</a>
+        <a class="${jsTabLink}" href="#estimate">Estimate of costs</a>
       </li>
     </ul>
-    <div id="builder" class="js-box box">
+    <div id="builder" class="${tabBody} box">
       <h3>CV-Builder</h3>
       <p>
       A resume builder with export in PDF format written with
       the following technologies:
       </p>
       <ul>
-        <li>React.js</li>
-        <li>Redux.js</li>
-        <li>Redux Saga</li>
-        <li>Typescript</li>
-        <li>Styled components</li>
-        <li>Puppeteer</li>
-        <li>Node.js/Express.js</li>
-        <li>Webpack.js</li>
-        <li>Jest</li>
+        <li class="${stackLi} d-hide">React.js</li>
+        <li class="${stackLi} d-hide">Redux.js</li>
+        <li class="${stackLi} d-hide">Redux Saga</li>
+        <li class="${stackLi} d-hide">Typescript</li>
+        <li class="${stackLi} d-hide">Styled components</li>
+        <li class="${stackLi} d-hide">Puppeteer</li>
+        <li class="${stackLi} d-hide">Node.js/Express.js</li>
+        <li class="${stackLi} d-hide">Webpack.js</li>
+        <li class="${stackLi} d-hide">Jest</li>
+        <li class="${stackLi} d-hide">ES6</li>
       </ul>
       <hr>
       <div class="links">
         <a target="_blank" rel="noopener" href="${urlCvBuilder}">Source code</a>
       </div>
     </div>
-    <div id="vuemmerce" class="js-box box d-none">
+    <div id="vuemmerce" class="${tabBody} box d-hide">
       <h3>Vuemmerce | E-commerce template</h3>
       <p>Technologies used:</p>
       <ul>
-        <li>Vue.js</li>
-        <li>Nuxt.js</li>
-        <li>Vuex</li>
-        <li>Bulma framework</li>
+        <li class="${stackLi} d-hide">Vue.js</li>
+        <li class="${stackLi} d-hide">ES6</li>
+        <li class="${stackLi} d-hide">Nuxt.js</li>
+        <li class="${stackLi} d-hide">Vuex</li>
+        <li class="${stackLi} d-hide">Bulma framework</li>
       </ul>
       <hr>
       <div class="links">
@@ -87,12 +66,13 @@ export const ProjectsSection: string = `
         <a target="_blank" rel="noopener" href="${urlDemoVuemmerce}">Demo</a>
       </div>
     </div>
-    <div id="estimate" class="js-box box d-none">
+    <div id="estimate" class="${tabBody} box d-hide">
       <h3>Estimate of costs</h3>
       <p>Technologies used:</p>
       <ul>
-        <li>Vanilla JS</li>
-        <li>Grid CSS</li>
+        <li class="${stackLi} d-hide">Vanilla JS</li>
+        <li class="${stackLi} d-hide">ES6</li>
+        <li class="${stackLi} d-hide">Grid CSS</li>
       </ul>
       <hr>
       <div class="links">
@@ -103,3 +83,44 @@ export const ProjectsSection: string = `
     </div>
   </div>
 </section>`;
+
+function tabsClickHandler() {
+  document.addEventListener('click', (e: any) => {
+    if (!e.target.matches(`.${jsTabLink}`)) return;
+    e.preventDefault();
+    const target = e.target;
+
+    document.querySelectorAll(`.${jsTabLink}`).forEach(el => {
+      el.classList.remove('active');
+    });
+
+    // if tab is hidden
+    if (document.querySelector('.active') === null) {
+      document.querySelectorAll(`.${tabBody}`).forEach(el => {
+        el.classList.add('d-hide');
+      });
+      stackListAnimationHandler(target.hash);
+      document.querySelector(target.hash).classList.remove('d-hide');
+      target.classList.add('active');
+    }
+  });
+}
+
+function stackListAnimationHandler(container: string) {
+  const projectStack = document.querySelectorAll(`${container} .${stackLi}`);
+
+  projectStack.forEach((item, index) => {
+    item.classList.remove(typeOfAnimation);
+    item.classList.add('d-hide');
+
+    setTimeout(() => {
+      item.classList.remove('d-hide');
+      item.classList.add(typeOfAnimation);
+    }, 300 * index);
+  });
+}
+
+export const handleDOM = (): void => {
+  stackListAnimationHandler('#builder');
+  tabsClickHandler();
+}
